eb0f184c40810dac8c473ff394c457facb1f0489
tomeeagle <tomeagleweb@gmail.com>
2025-01-07 17:07:17 +0000

    Add routes for people and products

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const { readFileSync } = require('fs');
+const path = require('path');
 
 // Get all files
 const homePage = readFileSync('./navbar-app/index.html');
@@ -7,20 +8,39 @@ const homeStyles = readFileSync('./navbar-app/styles.css');
 const homeImage = readFileSync('./navbar-app/logo.svg');
 const homeLogic = readFileSync('./navbar-app/browser-app.js');
 
+const { people, products } = require('./data');
+
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
+    const { url } = req;
+
+    console.log(req.url);
+
+    if (url === '/products') {
+        res.writeHead(200, { 'content-type': 'text/html' });
+        products?.forEach((product) => {
+            res.write(`<div>${product.name}</div>`);
+            res.write(`<div>${product.price}</div>`);
+            res.write(`<div>${product.desc}</div>`);
+            res.write(`<img src="${product.image}" alt="${product.name}" />`);
+        });
+        res.end();
+    } else if (url === '/people') {
+        res.writeHead(200, { 'content-type': 'application/json' });
+        res.write(JSON.stringify(people));
+        res.end();
+    } else if (url === '/') {
         res.writeHead(200, { 'content-type': 'text/html' });
         res.write(homePage);
         res.end();
-    } else if (req.url === '/styles.css') {
+    } else if (url === '/styles.css') {
         res.writeHead(200, { 'content-type': 'text/css' });
         res.write(homeStyles);
         res.end();
-    } else if (req.url === '/logo.svg') {
+    } else if (url === '/logo.svg') {
         res.writeHead(200, { 'content-type': 'image/svg+xml' });
         res.write(homeImage);
         res.end();
-    } else if(req.url === '/browser-app.js') {
+    } else if (url === '/browser-app.js') {
         res.writeHead(200, { 'content-type': 'text/javascript' });
         res.write(homeLogic);
         res.end();
