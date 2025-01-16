443805a1458a0319468e3a951f6105e96664ecf0
tomeeagle <tomeagleweb@gmail.com>
2025-01-16 10:56:19 +0000

    Add express routing

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,12 +1,32 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.send('<h1>HELLOW GANG</h1>');
+    // Return the index.html file from the navbar-app folder
+    res.sendFile(path.resolve(__dirname, 'navbar-app/index.html'));
+});
+
+// Routes for about, projects and contact pages
+app.get('/about', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'navbar-app/about.html'));
+});
+
+app.get('/projects', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'navbar-app/projects.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'navbar-app/contact.html'));
+});
+
+// Make sure we also return the css and js files requested by the index.html file
+app.use(express.static('./public'));
+
+app.all('*', (req, res) => {
+    res.status(404).send('Resource not found');
 });
 
 app.listen(3000, () => {
-    console.log('Server is listening on port 3000....');
+    console.log('Server is running on port 3000');
 });
