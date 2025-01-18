ae1db6ead39d2ffff4817f4b672bbfceed7d9f12
tomeeagle <tomeagleweb@gmail.com>
2025-01-18 10:32:10 +0000

    Serve all static files from public folder

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -2,26 +2,26 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-    // Return the index.html file from the navbar-app folder
-    res.sendFile(path.resolve(__dirname, 'navbar-app/index.html'));
-});
+// app.get('/', (req, res) => {
+//     // Return the index.html file from the ./public folder
+//     res.sendFile(path.resolve(__dirname, './public/index.html'));
+// });
 
-// Routes for about, projects and contact pages
-app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'navbar-app/about.html'));
-});
+// // Routes for about, projects and contact pages
+// app.get('/about', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './public/about.html'));
+// });
 
-app.get('/projects', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'navbar-app/projects.html'));
-});
+// app.get('/projects', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './public/projects.html'));
+// });
 
-app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'navbar-app/contact.html'));
-});
+// app.get('/contact', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, './public/contact.html'));
+// });
 
 // Make sure we also return the css and js files requested by the index.html file
-app.use(express.static('./navbar-app'));
+app.use(express.static('./public'));
 
 app.all('*', (req, res) => {
     res.status(404).send('Resource not found');
