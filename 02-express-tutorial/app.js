25886bfdc7ae3d09326284f21b697fb2f7b49776
tomeeagle <tomeagleweb@gmail.com>
2025-01-24 12:25:46 +0000

    POST with JSON. Fix form JS

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -5,25 +5,32 @@ const { people } = require('./data');
 
 // Static assets
 app.use(express.static('./methods-public'));
-app.use(morgan('tiny'));
 
 // parse form data
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Routes
 app.get('/api/people', (req, res) => {
     res.status(200).json({ success: true, data: people });
 });
 
-// Route for
+// Route for /api/postman/people POST
+app.post('/api/postman/people', (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        return res.status(400).json({ success: false, msg: 'Please provide name value' });
+    }
+    res.status(201).json({ success: true, data: [...people, {name}] });
+});
 
 // POST method route
-app.post('/api/people', function (req, res) {
-    console.log('BODY', req.body);
-    res.json({
-        message: 'POST request to the homepage',
-        data: people
-    });
+// Create a route to recieve JSON object with a name property
+app.post('/api/people', (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        return res.status(400).json({ success: false, msg: 'Please provide name value' });
+    }
+    res.status(201).json({ success: true, person: name });
 });
 
 // POST login route
@@ -31,13 +38,9 @@ app.post('/login', function (req, res) {
     // Get the form data from the request
     const { name } = req.body;
 
-    if (name) res.send(`<h1>Welcome ${name}</h1>`);
+    if (name) return res.send(`<h1>Welcome ${name}</h1>`);
     // If no name is provided, send an error message
     res.status(401).send('Please provide a name');
-
-
-     
-
 });
 
 app.listen(3000, () => {
